246ec2b2ed6013143caa82287909c89247731db6
Hugo Wainwright <wainwrighthugo@gmail.com>
2020-10-20 00:51:54 +0100

    added widgets for replay and export actions

--- src/FindReplays.js
+++ src/FindReplays.js
@@ -1,7 +1,15 @@
 import React from "react";
 import { ReplayTagTree } from "./ReplayTagTree";
 import { ReplayFilterAndSort } from "./ReplayFilterAndSort";
-import { Button, Intent, Classes } from "@blueprintjs/core";
+import {
+  Button,
+  Card,
+  Classes,
+  FormGroup,
+  H5,
+  HTMLSelect,
+  Intent,
+} from "@blueprintjs/core";
 import { IconNames } from "@blueprintjs/icons";
 
 import "./FindReplays.css";
@@ -112,12 +120,72 @@ export class FindReplays extends React.Component {
           </Button>
         </div>
         {this.state.resultsCounter === 0 ? null : (
-          <ReplayTagTree
-            key={this.state.resultsCounter}
-            className={this.state.loading ? Classes.SKELETON : ""}
-            {...this.props}
-            {...this.state}
-          />
+          <div
+            className={`FindReplays-results-container ${
+              this.state.loading ? Classes.SKELETON : ""
+            }`}
+          >
+            <ReplayTagTree
+              className={"FindReplays-tag-tree"}
+              key={this.state.resultsCounter}
+              {...this.props}
+              {...this.state}
+            />
+            <div className={"FindReplays-result-options-container"}>
+              <Card className={"FindReplays-result-options-card"}>
+                <H5>Replay actions</H5>
+                <Button fill={true} icon={IconNames.DOCUMENT}>
+                  View details{" "}
+                </Button>
+                <Button
+                  fill={true}
+                  intent={Intent.WARNING}
+                  icon={IconNames.DELETE}
+                >
+                  Forget replay
+                </Button>
+              </Card>
+              <Card className={"FindReplays-result-options-card"}>
+                <H5>Export actions</H5>
+                <FormGroup label={"Export replay(s) to"}>
+                  <HTMLSelect
+                    fill={true}
+                    options={[
+                      { label: "Temporary directory", value: "tempDir" },
+                      {
+                        label: "Choose directory...",
+                        value: "chooseDir",
+                        disabled: true,
+                      },
+                      {
+                        label: "Scelight",
+                        value: "scelight",
+                        disabled: true,
+                      },
+                      {
+                        label: "Sc2ReplayStats",
+                        value: "sc2replaystats",
+                        disabled: true,
+                      },
+                      {
+                        label: "Sc2ReplayStats",
+                        value: "sc2replaystats",
+                        disabled: true,
+                      },
+                    ]}
+                    value={"tempDir"}
+                  />
+                </FormGroup>
+                <Button
+                  fill={true}
+                  icon={IconNames.EXPORT}
+                  intent={Intent.PRIMARY}
+                >
+                  Export
+                </Button>
+              </Card>
+            </div>
+          </div>
         )}
       </div>
     );
