c7e1b267f4383338abf09a6130986d3ba6f12adf
Hugo Wainwright <wainwrighthugo@gmail.com>
2020-10-24 00:20:56 +0100

    refactor export actions card

--- src/ExportActionsCard.js
+++ src/ExportActionsCard.js
@@ -10,81 +10,122 @@ import {
 } from "@blueprintjs/core";
 import { IconNames } from "@blueprintjs/icons";
 import { AsyncUtils } from "./AsyncUtils";
-import React from "react";
+import React, { Fragment } from "react";
 
 import "./ExportActionsCard.css";
+
+function ExportToDirectoryButton(props) {
+  const {
+    disabled,
+    loading,
+    setLoading,
+    setOutputPath,
+    exportToDirectory,
+  } = props;
+
+  return (
+    <Button
+      fill={true}
+      icon={IconNames.EXPORT}
+      intent={Intent.PRIMARY}
+      loading={loading}
+      disabled={disabled}
+      onClick={async () => {
+        setLoading(true);
+
+        const [outputPath] = await Promise.all([
+          exportToDirectory(),
+          AsyncUtils.sleep(200),
+        ]);
+
+        setLoading(false);
+        setOutputPath(outputPath);
+      }}
+    >
+      Export
+    </Button>
+  );
+}
+
+function ExportOutputPathFormGroup(props) {
+  const { loading, outputPath } = props;
+
+  return outputPath ? (
+    <FormGroup
+      className={"ExportActionsCard-output-path-form-group"}
+      label="Export output"
+    >
+      <InputGroup
+        disabled={loading}
+        fill={true}
+        value={outputPath}
+        leftElement={<Icon icon={IconNames.FOLDER_CLOSE} />}
+        rightElement={
+          <Button
+            loading={loading}
+            icon={IconNames.DUPLICATE}
+            title={"Copy to clipboard"}
+            onClick={() => navigator.clipboard.writeText(outputPath)}
+          />
+        }
+      />
+    </FormGroup>
+  ) : null;
+}
+
+function ExportOptionFragment(props) {
+  const {
+    exportTarget,
+    disabled,
+    loading,
+    setLoading,
+    outputPath,
+    setOutputPath,
+    ...other
+  } = props;
+
+  switch (exportTarget) {
+    case "tempDir":
+      const { exportReplaysToTemporaryDirectory } = other;
+
+      return (
+        <Fragment>
+          <ExportToDirectoryButton
+            disabled={disabled}
+            loading={loading}
+            setLoading={setLoading}
+            setOutputPath={setOutputPath}
+            exportToDirectory={exportReplaysToTemporaryDirectory}
+          />
+          <ExportOutputPathFormGroup
+            loading={loading}
+            outputPath={outputPath}
+          />
+        </Fragment>
+      );
+    default:
+      return null;
+  }
+}
 
 export class ExportActionsCard extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      exportTarget: "tempDir",
+      exportOption: "tempDir",
       outputPath: null,
     };
   }
 
-  renderExportOption() {
-    const { disabled, loading, setLoading, ...other } = this.props;
-
-    switch (this.state.exportTarget) {
-      case "tempDir":
-        const { exportReplaysToTemporaryDirectory } = other;
-
-        return (
-          <div>
-            <Button
-              fill={true}
-              icon={IconNames.EXPORT}
-              intent={Intent.PRIMARY}
-              loading={loading}
-              disabled={disabled}
-              onClick={async () => {
-                setLoading(true);
-
-                const [outputPath] = await Promise.all([
-                  exportReplaysToTemporaryDirectory(),
-                  AsyncUtils.sleep(200),
-                ]);
-
-                setLoading(false);
-                this.setState({ outputPath: outputPath });
-              }}
-            >
-              Export
-            </Button>
-            {this.state.outputPath ? (
-              <FormGroup
-                className={"ExportActionsCard-output-path-form-group"}
-                label="Export output"
-              >
-                <InputGroup
-                  disabled={loading}
-                  fill={true}
-                  value={this.state.outputPath}
-                  leftElement={<Icon icon={IconNames.FOLDER_CLOSE} />}
-                  rightElement={
-                    <Button
-                      loading={loading}
-                      icon={IconNames.DUPLICATE}
-                      title={"Copy to clipboard"}
-                      onClick={() =>
-                        navigator.clipboard.writeText(this.state.outputPath)
-                      }
-                    />
-                  }
-                />
-              </FormGroup>
-            ) : null}
-          </div>
-        );
-      default:
-        return null;
-    }
-  }
-
   render() {
-    const { className } = this.props;
+    const {
+      className,
+      disabled,
+      loading,
+      setLoading,
+      exportReplaysToTemporaryDirectory,
+    } = this.props;
 
     return (
       <Card className={className}>
@@ -111,11 +152,21 @@ export class ExportActionsCard extends React.Component {
               },
             ]}
             onChange={(event) =>
-              this.setState({ exportTarget: event.currentTarget.value })
+              this.setState({ exportOption: event.currentTarget.value })
             }
           />
         </FormGroup>
-        {this.renderExportOption()}
+        <ExportOptionFragment
+          disabled={disabled}
+          loading={loading}
+          setLoading={setLoading}
+          exportTarget={this.state.exportOption}
+          outputPath={this.state.outputPath}
+          setOutputPath={(outputPath) =>
+            this.setState({ outputPath: outputPath })
+          }
+          exportReplaysToTemporaryDirectory={exportReplaysToTemporaryDirectory}
+        />
       </Card>
     );
   }
